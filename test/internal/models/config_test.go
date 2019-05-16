4bd52bf17d8445fefdb7bd961ad27e604a6d483c
Yeung Yiu Hung <hkclex@gmail.com>
2019-05-16 13:18:17 +0800

    feat(tests): value check using testify/assert

--- test/internal/models/config_test.go
+++ test/internal/models/config_test.go
@@ -5,10 +5,12 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	. "github.com/shoplineapp/captin/internal/models"
 )
 
-func TestAbs(t *testing.T) {
+func TestDecodeConfigurationJson(t *testing.T) {
 	fixture, err := ioutil.ReadFile("fixtures/config.json")
 	if err != nil {
 		panic(err)
@@ -16,35 +18,12 @@ func TestAbs(t *testing.T) {
 	subject := Configuration{}
 	json.Unmarshal(fixture, &subject)
 
-	if subject.CallbackURL != "http://callback_url/sync" {
-		t.Errorf("subject.CallbackURL = %s; want http://callback_url/sync", subject.CallbackURL)
-	}
-
-	if subject.ConfigID != "1234" {
-		t.Errorf("subject.ConfigID = %s; want 1234", subject.ConfigID)
-	}
-
-	if subject.Validate != "function(obj) { return !!obj.wapos_id }" {
-		t.Errorf("subject.Validate = %s; want function(obj) { return !!obj.wapos_id }", subject.Validate)
-	}
-
-	if subject.Throttle != "500ms" {
-		t.Errorf("subject.Throttle = %s; want 500ms", subject.Throttle)
-	}
-
-	if len(subject.Actions) != 6 {
-		t.Errorf("len(subject.Actions) = %d; want 6", 6)
-	}
-
-	if subject.Source != "core-api" {
-		t.Errorf("subject.Source = %s; want core-api", subject.Source)
-	}
-
-	if subject.Name != "sync_service" {
-		t.Errorf("subject.Name = %s; want sync_service", subject.Name)
-	}
-
-	if subject.IncludeDocument != false {
-		t.Errorf("subject.IncludeDocument = true; want false")
-	}
+	assert.Equal(t, subject.CallbackURL, "http://callback_url/sync")
+	assert.Equal(t, subject.ConfigID, "1234")
+	assert.Equal(t, subject.Validate, "function(obj) { return !!obj.wapos_id }")
+	assert.Equal(t, subject.Throttle, "500ms")
+	assert.Equal(t, len(subject.Actions), 6)
+	assert.Equal(t, subject.Source, "core-api")
+	assert.Equal(t, subject.Name, "sync_service")
+	assert.Equal(t, subject.IncludeDocument, false)
 }
