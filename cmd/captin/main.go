aabb0955ddd94c270c1ad0f400406c12c50ba7ab
Yeung Yiu Hung <hkclex@gmail.com>
2019-05-16 17:31:12 +0800

    feat(cli): add entry point for reading config

--- cmd/captin/main.go
+++ cmd/captin/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	models "github.com/shoplineapp/captin/internal/models"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("* Starting captin")
+	fmt.Println(os.Args[1:])
+
+	pwd, _ := os.Getwd()
+	path := os.Args[1:][0]
+	absPath := filepath.Join(pwd, path)
+
+	data, err := ioutil.ReadFile(absPath)
+
+	if err != nil {
+		fmt.Println("[Configuration] Failed to load file")
+		panic(err)
+	}
+
+	configs := []models.Configuration{}
+	json.Unmarshal(data, &configs)
+
+	mappedConfigs := models.NewConfigurationMapper(configs)
+
+	// TODO: Use Mapped Configuration for webhook logics
+	fmt.Printf("%+v\n", mappedConfigs.ActionMap)
 }
