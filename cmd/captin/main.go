f122e11e3b6bfdb4502a213bf639810d53591d1b
Yeung Yiu Hung <hkclex@gmail.com>
2019-05-21 18:40:50 +0800

    docs(example): update example

--- cmd/captin/main.go
+++ cmd/captin/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	core "github.com/shoplineapp/captin/core"
 	stores "github.com/shoplineapp/captin/internal/stores"
@@ -21,13 +23,26 @@ func main() {
 	configMapper := models.NewConfigurationMapperFromPath(absPath)
 	store := stores.NewMemoryStore()
 	captin := core.NewCaptin(*configMapper, store)
-	captin.Execute(models.IncomingEvent{
-		Key:        "product.update",
-		Source:     "core",
-		Payload:    map[string]interface{}{"field1": 1},
-		TargetType: "Product",
-		TargetId:   "product_id",
-	})
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		parsedInt, err := strconv.Atoi(text)
 
-	bufio.NewReader(os.Stdin).ReadBytes('\n')
+		if err == nil {
+			for i := 0; i < parsedInt; i++ {
+				captin.Execute(models.IncomingEvent{
+					Key:        "product.update",
+					Source:     "core",
+					Payload:    map[string]interface{}{"field1": 1},
+					TargetType: "Product",
+					TargetId:   "product_id",
+				})
+			}
+		} else {
+			os.Exit(0)
+		}
+	}
 }
