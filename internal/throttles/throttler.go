2bb6a2c05b19970fa65c17875c7d52d6a220d13a
Yeung Yiu Hung <hkclex@gmail.com>
2019-05-22 10:35:36 +0800

    test(throttler): add throttle tests

--- internal/throttles/throttler.go
+++ internal/throttles/throttler.go
@@ -27,13 +27,13 @@ func (t *Throttler) CanTrigger(id string) (bool, time.Duration, error) {
 	val, ok, duration, err := t.store.Get(id)
 
 	if err != nil {
-		return true, 0, err
+		return true, time.Duration(0), err
 	}
 	fmt.Println("[Throttler] Value: ", val)
 	if !ok {
 		fmt.Println("[Throttler] Create throttle in store with period, ", t.period)
 		t.store.Set(id, "1", t.period)
-		return true, 0, nil
+		return true, time.Duration(0), nil
 	}
 
 	return false, duration, nil
--- test/internal/throttles/throttler_test.go
+++ test/internal/throttles/throttler_test.go
@@ -1,10 +1,12 @@
 package throttles_test
 
 import (
+	"errors"
 	"testing"
 	"time"
 
 	interfaces "github.com/shoplineapp/captin/interfaces"
+	throttles "github.com/shoplineapp/captin/internal/throttles"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -18,7 +20,7 @@ type storeMock struct {
 // Get - Get value from store, return with remaining time
 func (s *storeMock) Get(key string) (string, bool, time.Duration, error) {
 	args := s.Called(key)
-	return args.String(0), args.Bool(1), args.Get(3).(time.Duration), args.Error(4)
+	return args.String(0), args.Bool(1), args.Get(2).(time.Duration), args.Error(3)
 }
 
 // Set - Set value into store with ttl
@@ -39,6 +41,53 @@ func (s *storeMock) Remove(key string) (bool, error) {
 	return args.Bool(0), args.Error(1)
 }
 
-func TestThrottler(t *testing.T) {
-	assert.True(t, true, true)
+func setup() (string, time.Duration, *storeMock) {
+	throttleID := "123"
+	throttlePeriod := time.Millisecond * 10
+
+	store := new(storeMock)
+	return throttleID, throttlePeriod, store
+}
+func TestThrottler_CreateStoreRecord(t *testing.T) {
+	throttleID, throttlePeriod, store := setup()
+
+	store.On("Get", throttleID).Return("", false, time.Millisecond*10, nil)
+	store.On("Set", throttleID, "1", throttlePeriod).Return(true, nil)
+
+	subject := throttles.NewThrottler(throttlePeriod, store)
+	result, duration, err := subject.CanTrigger(throttleID)
+	assert.True(t, result)
+	assert.Equal(t, time.Duration(0), duration)
+	assert.Nil(t, err)
+
+	store.AssertCalled(t, "Get", throttleID)
+	store.AssertCalled(t, "Set", throttleID, "1", throttlePeriod)
+}
+
+func TestThrottler_Reject(t *testing.T) {
+	throttleID, throttlePeriod, store := setup()
+
+	store.On("Get", throttleID).Return("1", true, time.Millisecond*10, nil)
+
+	subject := throttles.NewThrottler(throttlePeriod, store)
+	result, duration, err := subject.CanTrigger(throttleID)
+	assert.False(t, result)
+	assert.Equal(t, time.Millisecond*10, duration)
+	assert.Nil(t, err)
+
+	store.AssertCalled(t, "Get", throttleID)
+}
+
+func TestThrottler_Error(t *testing.T) {
+	throttleID, throttlePeriod, store := setup()
+
+	store.On("Get", throttleID).Return("", false, time.Duration(0), errors.New("some error"))
+
+	subject := throttles.NewThrottler(throttlePeriod, store)
+	result, duration, err := subject.CanTrigger(throttleID)
+	assert.True(t, result)
+	assert.Equal(t, time.Duration(0), duration)
+	assert.EqualError(t, err, "some error")
+
+	store.AssertCalled(t, "Get", throttleID)
 }
