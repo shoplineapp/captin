fd08b3dd57d1c7cb68a2b9ac6a408e8d6f35a441
Yeung Yiu Hung <hkclex@gmail.com>
2019-05-22 10:43:51 +0800

    test(store): move store mock to test/mocks package

---
+++ test/mocks/store_mock.go
@@ -0,0 +1,38 @@
+package mocks
+
+import (
+	"time"
+
+	"github.com/shoplineapp/captin/interfaces"
+	"github.com/stretchr/testify/mock"
+)
+
+// StoreMock - Mock of StoreInterface
+type StoreMock struct {
+	interfaces.StoreInterface
+	mock.Mock
+}
+
+// Get - Get value from store, return with remaining time
+func (s *StoreMock) Get(key string) (string, bool, time.Duration, error) {
+	args := s.Called(key)
+	return args.String(0), args.Bool(1), args.Get(2).(time.Duration), args.Error(3)
+}
+
+// Set - Set value into store with ttl
+func (s *StoreMock) Set(key string, value string, ttl time.Duration) (bool, error) {
+	args := s.Called(key, value, ttl)
+	return args.Bool(0), args.Error(1)
+}
+
+// Update - Update value for key
+func (s *StoreMock) Update(key string, value string) (bool, error) {
+	args := s.Called(key, value)
+	return args.Bool(0), args.Error(1)
+}
+
+// Remove - Remove value for key
+func (s *StoreMock) Remove(key string) (bool, error) {
+	args := s.Called(key)
+	return args.Bool(0), args.Error(1)
+}
