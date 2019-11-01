6bd72144fe9f21155eef61bfe450cd64e3e42017
Ray Su <ray.su@shoplineapp.com>
2019-11-02 00:21:17 +0800

    refactor(BeanstalkdSender): use log with fields and remove panic

--- internal/senders/beanstlakd_sender.go
+++ internal/senders/beanstlakd_sender.go
@@ -2,41 +2,57 @@ package senders
 
 import (
   "encoding/json"
-  models "github.com/shoplineapp/captin/models"
-  beanstalk "github.com/beanstalkd/go-beanstalk"
   "fmt"
   "time"
+
+  interfaces "github.com/shoplineapp/captin/interfaces"
+  models "github.com/shoplineapp/captin/models"
+  beanstalk "github.com/beanstalkd/go-beanstalk"
   log "github.com/sirupsen/logrus"
 )
 
+bLogger = log.WithFields(log.Fields{"class": "BeanstalkdSender"})
 
 // BeanstalkdSender - Send Event to beanstalkd
-type BeanstalkdSender struct{}
+type BeanstalkdSender struct {
+  interfaces.EventSenderInterface
+}
 
 // SendEvent - #BeanstalkdSender SendEvent
 func (c *BeanstalkdSender) SendEvent(e models.IncomingEvent, d models.Destination) error {
-  host := fmt.Sprintf("%s:%s", e.Control["beanstalkd_host"], e.Control["beanstalkd_port"])
-  conn, err := beanstalk.Dial("tcp", host)
-
+  conn, err := beanstalk.Dial("tcp", e.Control["beanstalkd_host"])
   if err != nil {
-    log.Info("Beanstalk create connection failed: ", err)
-    panic(err)
+    bLogger.WithFields(log.Fields{
+      "error": err
+    }).Error("Beanstalk create connection failed.")
+    return err
   }
 
   tubeName := fmt.Sprintf("%s.%s", e.Control["tube_namespace"], e.Control["queue_name"])
   conn.Tube = beanstalk.Tube { Conn: conn, Name: tubeName }
+
   jobBody, err := json.Marshal(e.Payload)
   if err != nil {
-    log.Info(fmt.Sprintf("* Beanstalkd job payload format invalid: %s", string(jobBody)))
-    panic(err)
+    bLogger.WithFields(log.Fields{
+      "error": err
+    }).Error("Beanstalkd job payload format invalid.")
+    return err
   }
+
   pri, delay, ttr := uint32(1), time.Duration(0), time.Duration(0)
   id, err := conn.Put(jobBody, pri, delay, ttr)
   if err != nil {
-    log.Info(fmt.Sprintf("* Beanstalk client put job failed: %s", string(jobBody)))
-    panic(err)
+    bLogger.WithFields(log.Fields{
+      "error": err
+    }).Error("Beanstalk client put job failed.")
+    return err
   }
-  log.Info(fmt.Sprintf("Enqueue job id: %d, body: %s", id, string(jobBody)))
+
+  bLogger.WithFields(log.Fields{
+    "id": id
+    "jobBody": string(jobBody)
+  }).Info("Enqueue job.")
+
   defer conn.Close()
   return nil
 }
