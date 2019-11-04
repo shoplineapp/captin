d1ec504f4cae6d80742a3b55e1e04abf27068140
Philip Yu <ht.yu@me.com>
2019-11-04 10:48:05 +0800

    feat(dispatcher): add logic to check control timestamp for preventing update of event data with old value

--- test/internal/outgoing/dispatcher_test.go
+++ test/internal/outgoing/dispatcher_test.go
@@ -130,6 +130,32 @@ func TestDispatchEvents_Throttled_DelaySend(t *testing.T) {
 	store.AssertCalled(t, "Remove", throttleID)
 }
 
+func TestDispatchEvents_Throttled_SkipUpdatingValue(t *testing.T) {
+	store, sender, dispatcher, throttler := setup("fixtures/config.single.json")
+	throttleID := "product.update.service_one.product_id-data"
+	throttlePeriod := time.Millisecond * 700 * 2
+
+	sender.On("SendEvent", mock.Anything, mock.Anything).Return(nil)
+	store.On("Get", throttleID).Return(`{"event_key":"product.update","source":"core","payload":{"field1":1},"control":{"ts":"99999999999999"},"target_type":"Product","target_id":"product_id"}`, true, throttlePeriod, nil)
+	store.On("Set", mock.Anything, mock.Anything, mock.Anything).Return(true, nil)
+	store.On("Update", mock.Anything, mock.Anything).Return(true, nil)
+	throttler.On("CanTrigger", mock.Anything, mock.Anything).Return(false, time.Millisecond*700, nil)
+
+	dispatcher.Dispatch(models.IncomingEvent{
+		Key:        "product.update",
+		Source:     "core",
+		Payload:    map[string]interface{}{"field1": 2},
+		Control:    map[string]interface{}{"ts": time.Now().Unix()},
+		TargetType: "Product",
+		TargetId:   "product_id",
+	}, store, throttler)
+
+	// Expecting update of store is skipped due to older timestamp from new incoming event
+	sender.AssertNumberOfCalls(t, "SendEvent", 0)
+	store.AssertNumberOfCalls(t, "Update", 0)
+	store.AssertCalled(t, "Get", throttleID)
+}
+
 func TestDispatchEvents_Throttled_UpdatePayload(t *testing.T) {
 	store, sender, dispatcher, throttler := setup("fixtures/config.single.json")
 	throttleID := "product.update.service_one.product_id-data"
